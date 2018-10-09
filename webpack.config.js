2e8b546c37a19049d9f2196bb724dd5f912b9f60
andyygit <android.bartos@gmail.com>
2018-10-09 15:42:02 +0300

    Add files via upload
    Initial commit, setup finished

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+var path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+var miniCSS = new MiniCssExtractPlugin({
+    filename: 'main.css'
+});
+
+module.exports = {
+    entry: './src/js/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/env']
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'sass-loader'
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        miniCSS
+    ]
+};
