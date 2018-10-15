eefcbe4877b6c4cac1a684c258202b87e2b14925
andyygit <android.bartos@gmail.com>
2018-10-15 14:33:31 +0300

    Add files via upload

--- static/bundle.js
+++ static/bundle.js
@@ -1,13 +1,25 @@
 window.onload = function () {
     var menuItem = document.querySelectorAll('nav a')[0];
-    menuItem.addEventListener('click', function () {
+    var nav = document.querySelectorAll('nav')[0];
+    var btn = document.getElementById('hamburger');
+    menuItem.addEventListener('click', function (event) {
+        event.preventDefault();
         menuItem.nextElementSibling.classList.toggle('hide');
     });
-    menuItem.addEventListener('focusout', function () {
+    menuItem.addEventListener('focusout', function (event) {
+        event.preventDefault();
         if (menuItem.nextElementSibling.classList.contains('hide')) {
             return;
         } else {
             menuItem.nextElementSibling.classList.toggle('hide');
+        }
+    });
+    btn.addEventListener('click', function(event) {
+        event.preventDefault();
+        if (nav.style.display == 'none') {
+            nav.style.display = 'block';
+        } else {
+            nav.style.display = 'none';
         }
     });
 };
